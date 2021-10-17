f9509840d3cdfbaf04af91cff338e38b2495b5c7
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2021-10-17 19:48:29 -0300

    adicao de exercicios bonus

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/ex7/scripts.js
@@ -0,0 +1,39 @@
+function contador(array){
+  let result = {};
+  for(let i = 0; i < array.length; i += 1){
+    const fruta = array[i];
+    if(!result[fruta]){
+      result[fruta] = 0;
+    }
+    result[fruta] += 1;
+  }
+  const quant = [];
+  for (fruta in result){
+    let mensagem = `${result[fruta]} ${fruta}`;
+    if (result[fruta] > 1){
+      mensagem += 's';
+    }
+    quant.push(mensagem);
+  }
+  console.log(`Sua cesta possui: ${quant.join(', ')}.`);
+}
+const basket = [
+  'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
+  'Jaca', 'Pera', 'Melancia', 'Uva', 'Laranja', 'Melancia',
+  'Banana', 'Uva', 'Pera', 'Abacate', 'Laranja', 'Abacate',
+  'Banana', 'Melancia', 'Laranja', 'Laranja', 'Jaca', 'Uva',
+  'Banana', 'Uva', 'Laranja', 'Pera', 'Melancia', 'Uva',
+  'Jaca', 'Banana', 'Pera', 'Abacate', 'Melancia', 'Melancia',
+  'Laranja', 'Pera', 'Banana', 'Jaca', 'Laranja', 'Melancia',
+  'Abacate', 'Abacate', 'Pera', 'Melancia', 'Banana', 'Banana',
+  'Abacate', 'Uva', 'Laranja', 'Banana', 'Abacate', 'Uva',
+  'Uva', 'Abacate', 'Abacate', 'Melancia', 'Uva', 'Jaca',
+  'Uva', 'Banana', 'Abacate', 'Banana', 'Uva', 'Banana',
+  'Laranja', 'Laranja', 'Jaca', 'Jaca', 'Abacate', 'Jaca',
+  'Laranja', 'Melancia', 'Pera', 'Jaca', 'Melancia', 'Uva',
+  'Abacate', 'Jaca', 'Jaca', 'Abacate', 'Uva', 'Laranja',
+  'Pera', 'Melancia', 'Jaca', 'Pera', 'Laranja', 'Jaca',
+  'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
+  'Banana', 'Pera', 'Abacate', 'Uva',
+];
+contador(basket)
