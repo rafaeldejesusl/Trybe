9e07d48df748c6cb01adc0a02afa14c6589bd5c5
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2021-11-22 19:15:03 -0300

    Atualizaçao dos exercicios

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/ex3/scripts.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/ex3/scripts.js
@@ -4,10 +4,13 @@ const names = [
   'Abeladerco', 'Adieidy', 'Alarucha',
 ];
 
+// function containsA() {
+//   let names1 = names.map((name) => name.toLowerCase());
+//   let rep = names1.map((name) => name.split('a').length - 1);
+//   console.log(rep);
+//   return rep.reduce((total, item) => total = total + item, 0);
+// }
 function containsA() {
-  let names1 = names.map((name) => name.toLowerCase());
-  let rep = names1.map((name) => name.split('a').length - 1);
-  console.log(rep);
-  return rep.reduce((total, item) => total = total + item, 0);
+  return names.reduce((total, word)=> total += word.split('').reduce((rep, letter)=> letter === 'a' || letter === 'A' ? rep += 1 : rep, 0), 0);
 }
 console.log(containsA());
--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/ex4/scripts.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/ex4/scripts.js
@@ -4,8 +4,8 @@ const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
 function studentAverage() {
   let media = grades.map((book) => book.reduce((total, grade) => total = total + grade, 0) / book.length);
   let result = students.map((student, index) => {
-    return {student: student, average: media[index]}
+    return {student, average: media[index]}
   })
-   return result;
+  return result;
 }
 console.log(studentAverage());
