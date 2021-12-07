eebea07882f42828cd7d881f74145adea61c59eb
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2021-12-07 19:14:27 -0300

    Exercicios de introdução a React

---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/ex2/exemplo2/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <Content />
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
